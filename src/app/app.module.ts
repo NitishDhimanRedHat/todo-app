43d0174adf27ca67a6a8ff18a1e18bc102103ef3
Nitish Dhiman <nitishdhiman930@gmail.com>
2020-12-16 15:22:27 +0530

    creating components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
